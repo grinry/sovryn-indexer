76a641d4d89bc5383711612125a028964c5dc097
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2024-06-04 16:39:12 +0300

    chore: add read-only mode

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import 'config';
+/* eslint-disable import/order */
+import config from 'config';
 import 'utils/shutdown';
 
 import { startApp } from 'app';
 import { startCrontab } from 'crontab';
+import { logger } from 'utils/logger';
 
 startApp();
-startCrontab();
+
+if (!config.readOnly) {
+  logger.info('Running in read-write mode. Starting crontab...');
+  startCrontab();
+}
