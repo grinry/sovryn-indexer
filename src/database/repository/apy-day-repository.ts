a097a1d083d21c18b23f22e1dd43ed9a47e1eac3
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2024-06-14 13:45:27 +0300

    feat: endpoints for amm pool data

--- src/database/repository/apy-day-repository.ts
+++ src/database/repository/apy-day-repository.ts
@@ -1,7 +1,10 @@
-import { sql } from 'drizzle-orm';
+import dayjs from 'dayjs';
+import { and, asc, desc, eq, gte, sql } from 'drizzle-orm';
 
 import { db } from 'database/client';
 import { ammApyDays, NewAmmApyDay } from 'database/schema';
+
+const DEFAULT_DATA_RANGE = 7; // 7 days
 
 export const apyDayRepository = {
   storeItems: (data: NewAmmApyDay[]) =>
@@ -18,4 +21,60 @@ export const apyDayRepository = {
           btcVolume: sql`excluded.btc_volume`,
         },
       }),
+  getAllPoolData: (chainId: number, days = DEFAULT_DATA_RANGE) =>
+    db
+      .select({
+        pool: ammApyDays.pool,
+        poolToken: ammApyDays.poolToken,
+        date: ammApyDays.date,
+        balanceBtc: ammApyDays.balanceBtc,
+        rewardsApy: ammApyDays.rewardsApy,
+        feeApy: ammApyDays.feeApy,
+        totalApy: ammApyDays.totalApy,
+        btcVolume: ammApyDays.btcVolume,
+      })
+      .from(ammApyDays)
+      .where(and(eq(ammApyDays.chainId, chainId), gte(ammApyDays.date, dayjs().subtract(days, 'days').toDate())))
+      .orderBy(asc(ammApyDays.date)),
+  getOnePoolData: (chainId: number, pool: string, days = DEFAULT_DATA_RANGE) =>
+    db
+      .select({
+        pool: ammApyDays.pool,
+        poolToken: ammApyDays.poolToken,
+        date: ammApyDays.date,
+        balanceBtc: ammApyDays.balanceBtc,
+        rewardsApy: ammApyDays.rewardsApy,
+        feeApy: ammApyDays.feeApy,
+        totalApy: ammApyDays.totalApy,
+        btcVolume: ammApyDays.btcVolume,
+      })
+      .from(ammApyDays)
+      .limit(2)
+      .where(
+        and(
+          eq(ammApyDays.chainId, chainId),
+          eq(ammApyDays.pool, pool),
+          gte(ammApyDays.date, dayjs().subtract(days, 'days').toDate()),
+        ),
+      )
+      .orderBy(desc(ammApyDays.date)),
+  getLastPoolApy: (chainId: number, pool: string) =>
+    db
+      .select({
+        pool: ammApyDays.pool,
+        poolToken: ammApyDays.poolToken,
+        date: ammApyDays.date,
+        balanceBtc: ammApyDays.balanceBtc,
+        rewardsApy: ammApyDays.rewardsApy,
+        feeApy: ammApyDays.feeApy,
+        totalApy: ammApyDays.totalApy,
+        btcVolume: ammApyDays.btcVolume,
+      })
+      .from(ammApyDays)
+      .where(and(eq(ammApyDays.chainId, chainId), eq(ammApyDays.pool, pool)))
+      .orderBy(asc(ammApyDays.date))
+      .execute()
+      .then((items) =>
+        items.length ? items.filter((item) => item.date.getUTCDay() === items[0].date.getUTCDay()) : [],
+      ),
 };
