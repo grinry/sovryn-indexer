f0dd63cff2ec01e689de95ee87a53c254b536d95
Pietro <74987028+pietro-maximoff@users.noreply.github.com>
2024-07-16 13:24:51 +0200

    SOV-4213: Sovryn Indexer Rate limiting implementation (#13)
    * chore: add rate limiting
    
    * chore: fixes from comments

---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import Redis from 'ioredis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+import config from 'config';
+import { HttpError } from 'utils/custom-error';
+
+interface RateLimiterOptions {
+  keyPrefix: string;
+  points: number;
+  duration: number;
+}
+
+const redisClient = new Redis(config.redisCacheUrl);
+
+const createRateLimiterMiddleware = (options: RateLimiterOptions) => {
+  const rateLimiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: `rm--${options.keyPrefix}`,
+    points: options.points,
+    duration: options.duration,
+  });
+
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const clientIp = req.ip;
+    try {
+      await rateLimiter.consume(clientIp);
+      next();
+    } catch (err) {
+      throw new HttpError(429, 'Too Many Requests');
+    }
+  };
+};
+
+export default createRateLimiterMiddleware;
