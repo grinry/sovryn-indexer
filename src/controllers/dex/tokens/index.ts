a4e298f614996d2609c503f914a5a72ba20e3c5c
pietro-maximoff <pietro.maxim.official@protonmail.com>
2024-10-22 11:55:29 +0200

    chore: update dex endpoints

--- src/controllers/dex/tokens/index.ts
+++ src/controllers/dex/tokens/index.ts
@@ -1,52 +1,89 @@
-import { and, eq } from 'drizzle-orm';
-import { isNotNull } from 'drizzle-orm';
+import { and, eq, sql, isNotNull } from 'drizzle-orm';
+import { alias } from 'drizzle-orm/pg-core';
 import { Router, Request, Response } from 'express';
+import { bignumber } from 'mathjs';
 
 import { DEFAULT_CACHE_TTL } from 'config/constants';
 import { db } from 'database/client';
+import { chains } from 'database/schema/chains';
 import { tokens } from 'database/schema/tokens';
+import { getLastPrices } from 'loader/price';
+import { validateChainId } from 'middleware/network-middleware';
 import { maybeCacheResponse } from 'utils/cache';
-import { toResponse } from 'utils/http-response';
+import { toPaginatedResponse, toResponse } from 'utils/http-response';
+import { prettyNumber } from 'utils/numbers';
+import { createApiQuery, OrderBy, validatePaginatedRequest } from 'utils/pagination';
 import { asyncRoute } from 'utils/route-wrapper';
 
 const router = Router();
 
-// indexer.sovryn.app/dex/30/tokens
+// Endpoint to fetch tradeable tokens with price
 router.get(
   '/',
-  asyncRoute(async (req: Request, res: Response) =>
-    maybeCacheResponse(
-      res,
-      `/dex/${req.network.chainId}/tokens`,
-      async () => {
-        const chainId = req.network.chainId;
+  asyncRoute(async (req: Request, res: Response) => {
+    const chainId = validateChainId(req, true);
+    const p = validatePaginatedRequest(req);
 
-        const tokensData = await db
+    return maybeCacheResponse(
+      res,
+      `/dex/${chainId}/tokens`,
+      async () => {
+        const quoteToken = alias(tokens, 'quote_token');
+        const chain = alias(chains, 'chain');
+
+        const tokenQuery = db
           .select({
+            id: tokens.id,
             symbol: tokens.symbol,
             name: tokens.name,
             decimals: tokens.decimals,
             chainId: tokens.chainId,
             address: tokens.address,
             logoUrl: tokens.logoUrl,
-            usdPrice: tokens.usdPrice,
+            stablecoinId: sql<number>`${quoteToken.id}`.as('stablecoinId'),
           })
           .from(tokens)
-          .where(and(eq(tokens.chainId, chainId), isNotNull(tokens.tradeableSince)))
-          .execute();
+          .where(
+            and(
+              chainId ? eq(tokens.chainId, chainId) : undefined,
+              Boolean(req.query.spam) ? undefined : eq(tokens.ignored, false),
+              isNotNull(tokens.tradeableSince),
+            ),
+          )
+          .innerJoin(chain, eq(tokens.chainId, chain.id))
+          .innerJoin(quoteToken, and(eq(quoteToken.chainId, chain.id), eq(quoteToken.address, chain.stablecoinAddress)))
+          .$dynamic();
 
-        return tokensData;
+        const api = createApiQuery('address', OrderBy.asc, (key) => tokens[key], p);
+        const items = await api.applyPagination(tokenQuery).execute();
+
+        // Fetch the latest prices for tokens
+        const lastPrices = await getLastPrices();
+
+        return api.getMetadata(
+          items.map((item) => {
+            const lastUsdPrice = lastPrices.find((price) => price.tokenId === item.id);
+            const price = bignumber(lastUsdPrice?.value ?? 0);
+            return {
+              ...item,
+              usdPrice: prettyNumber(price),
+              usdPriceDate: lastUsdPrice?.updatedAt ?? lastUsdPrice?.tickAt ?? null,
+              id: undefined,
+              stablecoinId: undefined,
+            };
+          }),
+        );
       },
       DEFAULT_CACHE_TTL,
-    ).then((data) => res.json(toResponse(data))),
-  ),
+    ).then((data) => res.json(toPaginatedResponse(data)));
+  }),
 );
 
-// indexer.sovryn.app/dex/30/tokens/available
+// Endpoint to fetch all available tokens (including non-tradeable)
 router.get(
   '/available',
-  asyncRoute(async (req: Request, res: Response) =>
-    maybeCacheResponse(
+  asyncRoute(async (req: Request, res: Response) => {
+    return maybeCacheResponse(
       res,
       `/dex/${req.network.chainId}/tokens/available`,
       async () => {
@@ -60,7 +97,6 @@ router.get(
             chainId: tokens.chainId,
             address: tokens.address,
             logoUrl: tokens.logoUrl,
-            usdPrice: tokens.usdPrice,
           })
           .from(tokens)
           .where(eq(tokens.chainId, chainId))
@@ -69,8 +105,8 @@ router.get(
         return tokensData;
       },
       DEFAULT_CACHE_TTL,
-    ).then((data) => res.json(toResponse(data))),
-  ),
+    ).then((data) => res.json(toResponse(data)));
+  }),
 );
 
 export default router;
